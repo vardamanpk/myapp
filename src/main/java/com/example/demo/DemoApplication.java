2019cf6a7866c86531e8488bde7332c696bfb02e
Vardaman PK <vapk@paypal.com>
2017-07-15 18:33:28 +0530

    fix packages

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import hello.CoinBase;
-import hello.EthexIndia;
-import hello.PriceResponse;
 
 import org.springframework.stereotype.*;
 
